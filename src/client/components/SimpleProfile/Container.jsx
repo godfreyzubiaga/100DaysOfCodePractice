96dcf6296eca28deef18a8ea9a6ea9da4d2147d8
Godfrey Zubiaga <godfreyzubiaga@gmail.com>
2019-01-09 21:46:24 +0800

    add Container component for Profile

---
+++ src/client/components/SimpleProfile/Container.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import { oneOfType, node, arrayOf } from 'prop-types';
+
+const StyledDiv = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 80%;
+  width: 80%;
+  margin: auto;
+  border: 1px solid white;
+`;
+
+const Container = ({ children }) => <StyledDiv>{children}</StyledDiv>;
+
+Container.propTypes = {
+  children: oneOfType([arrayOf(node), node]).isRequired,
+};
+
+export default Container;
